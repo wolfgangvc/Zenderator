ea4ab139472c57a21cabb7c606a72825d933f4a9
Matthew Baggett <matthew@baggett.me>
2016-06-29 15:01:19 +0100

    Working DI for services

--- src/Abstracts/Service.php
+++ src/Abstracts/Service.php
@@ -2,12 +2,7 @@
 namespace Zenderator\Abstracts;
 
 abstract class Service
-{
-    public function __construct(\Slim\Container $container)
-    {
-        $this->container = $container;
-    }
-    
+{    
     abstract function getNewModelInstance();
 
     abstract function getTermPlural() : string;
